7c146a44a007173cf8a83ea49d944b41df245341
Eric Tsang <surplus.et@gmail.com>
2016-01-29 04:19:24 -0800

    make ip addresses permanent
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- host.sh
+++ host.sh
@@ -1,4 +1,5 @@
-# run on machines on the LAN behind the firewall gateway
+# run on hosts on LAN
+# assumes appropriate NICs are enabled / disabled
 
 ### configuratoin ###
 
@@ -6,18 +7,18 @@
 NETWORK_INTERFACE="eth0"
 
 # address of this machine on the LAN
-HOST_ADDRESS="192.168.0.1"
+HOST_ADDRESS="10.0.0.1"
 
 # subnet address of this LAN
-SUBNET_ADDRESS="192.168.0.0/24"
+SUBNET_ADDRESS="10.0.0.0/24"
 
 # LAN's gateway to Internet
-GATEWAY_ADDRESS="192.168.0.0"
+GATEWAY_ADDRESS="10.0.0.0"
 
 ### code - do not touch! ###
 
 # configure network properties
-ip addr add $HOST_ADDRESS dev $NETWORK_INTERFACE
-ip route add $SUBNET_ADDRESS dev $NETWORK_INTERFACE
-ip route add default via $GATEWAY_ADDRESS dev $NETWORK_INTERFACE
+ip addr replace $HOST_ADDRESS dev $NETWORK_INTERFACE valid_lft forever preferred_lft forever
+ip route replace $SUBNET_ADDRESS dev $NETWORK_INTERFACE proto static
+ip route replace default via $GATEWAY_ADDRESS dev $NETWORK_INTERFACE proto static
 
