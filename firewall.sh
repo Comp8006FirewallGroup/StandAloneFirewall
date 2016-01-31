1ab791c0102976c3dc38bbb6c540402dd05e0e35
Eric Tsang <surplus.et@gmail.com>
2016-01-30 23:06:43 -0800

    udp client, tcp client, and in/out bound icmp complete
    * allow user to specify allowed remote UDP servers (may need to add state rules)
    * allow user to specify allowed remote TCP servers
    * allow user to specify allowed inbound ICMP packets
    * allow user to specify allowed outbound ICMP packets
    
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- firewall.sh
+++ firewall.sh
@@ -1,51 +1,45 @@
 # !/bin/sh
-echo "# !/bin/sh"
 
 ### configuration ###
-echo "### configuration ###"
 
 # general
-echo "# general"
 IPT="iptables"
-ALLOWED_ICMP_SERVICES="???"
-
 PS4='$LINENO '
 
 # network interfaces
-echo "# network interfaces"
 WAN_NIC="wlan0"
 LAN_NIC="eth0"
 
 # addresses
-echo "# addresses"
 HOST_ADDR="192.168.1.74"
 SUBNET_ADDR="10.210.0.0/24"
 DHCP_SERVERS="192.168.1.254"
-NAME_SERVERS="8.8.8.8"
+DNS_SERVERS="8.8.8.8"
 
-# ports
-echo "# ports"
-UNPRIV_PORTS="1024:65535"
-PRIV_PORTS="0:1023"
+# allowed ICMP packet types
+INBOUND_ICMP_TYPES="0"
+OUTBOUND_ICMP_TYPES="8"
 
+# allowed TCP ports
 LOCAL_TCP_SVRS="22 80"
 REMOTE_TCP_SVRS="22 80 443"
 
-LOCAL_UDP_SERVERS="22 80"
-REMOTE_UDP_SERVERS="22 80 443"
+# allowed UDP ports
+LOCAL_UDP_SVRS="22 80"
+REMOTE_UDP_SVRS="53"
 
 ### implementation - do not touch! ###
-echo "### implementation - do not touch! ###"
 
 # addresses
-echo "# addresses"
-LOOPBACK="127.0.0.1"
 ANY_ADDR="0.0.0.0/0"
 BROADCAST_SRC_ADDR="0.0.0.0"
 BROADCAST_DEST_ADDR="255.255.255.255"
 
+# ports
+UNPRIV_PORTS="1024:65535"
+PRIV_PORTS="0:1023"
+
 # reset firewall
-echo "# reset firewall"
 $IPT -F
 $IPT -t nat -F
 $IPT -t mangle -F
@@ -63,18 +57,16 @@ then
 fi
 
 # set default chain policies
-echo "# set default chain policies"
 $IPT -P INPUT DROP
 $IPT -P OUTPUT DROP
 $IPT -P FORWARD DROP
 
 # enable forwarding and masquerading
 # linuxpoison.blogspot.ca/2009/02/how-to-configure-linux-as-internet.html
-$IPT -t nat -A POSTROUTING -o $WAN_NETWORK_INTERFACE -j MASQUERADE
+$IPT -t nat -A POSTROUTING -o $WAN_NIC -j MASQUERADE
 
 # create user chains
-echo "# create user chains"
-USER_CHAINS="DHCP DNS ICMP TCP_SVR TCP_CLNT"
+USER_CHAINS="DHCP DNS ICMP TCP_SVR TCP_CLNT UDP_CLNT"
 for CHAIN in $USER_CHAINS
 do
 	$IPT -N $CHAIN
@@ -82,7 +74,6 @@ do
 done
 
 # enable DHCP traffic to DHCP servers
-echo "# enable DHCP traffic to DHCP servers"
 for SVR in $DHCP_SERVERS
 do
 	$IPT -A OUTPUT -o $WAN_NIC -p udp \
@@ -103,28 +94,46 @@ do
 				-j DHCP
 done
 
-for SVR in $NAME_SERVERS
+# enable DNS traffic to DNS servers
+for SVR_ADDR in $DNS_SERVERS
 do
-	$IPT -A OUTPUT -o $WAN_NIC -p udp \
-				-d $SVR --dport 53 \
-				-s $HOST_ADDR --sport $UNPRIV_PORTS \
-				-j DNS
-	$IPT -A INPUT -i $WAN_NIC -p udp \
-				-s $SVR --sport 53 \
-				-d $HOST_ADDR --dport $UNPRIV_PORTS \
-				-j DNS
+	# $IPT -A OUTPUT -o $WAN_NIC -p udp \
+	# 			-d $SVR_ADDR --dport 53 \
+	# 			-s $HOST_ADDR --sport $UNPRIV_PORTS \
+	# 			-j DNS
+	# $IPT -A INPUT -i $WAN_NIC -p udp \
+	# 			-s $SVR_ADDR --sport 53 \
+	# 			-d $HOST_ADDR --dport $UNPRIV_PORTS \
+	# 			-j DNS
 	$IPT -A FORWARD -p udp \
-				-o $WAN_NIC -d $SVR --dport 53 \
 				-i $LAN_NIC -s $SUBNET_ADDR --sport $UNPRIV_PORTS \
+				-o $WAN_NIC -d $SVR_ADDR --dport 53 \
 				-j DNS
 	$IPT -A FORWARD -p udp \
-				-i $WAN_NIC -s $SVR --sport 53 \
+				-i $WAN_NIC -s $SVR_ADDR --sport 53 \
 				-o $LAN_NIC -d $SUBNET_ADDR --dport $UNPRIV_PORTS \
 				-j DNS
 done
 
+# enable inbound ICMP traffic based on type
+for ICMP_TYPE in $INBOUND_ICMP_TYPES
+do
+	$IPT -A FORWARD -p icmp --icmp-type $ICMP_TYPE \
+				-i $WAN_NIC -s $ANY_ADDR \
+				-o $LAN_NIC -d $SUBNET_ADDR \
+				-j ICMP
+done
+
+# enable outbound ICMP traffic based on type
+for ICMP_TYPE in $OUTBOUND_ICMP_TYPES
+do
+	$IPT -A FORWARD -p icmp --icmp-type $ICMP_TYPE \
+				-i $LAN_NIC -s $SUBNET_ADDR \
+				-o $WAN_NIC -d $ANY_ADDR \
+				-j ICMP
+done
+
 # enable inbound TCP traffic to local TCP servers - TODO do some kind of port forwarding...
-echo "# enable inbound TCP traffic to local TCP servers"
 for SVR_PORT in $LOCAL_TCP_SVRS
 do
 	$IPT -A INPUT -i $WAN_NIC -p tcp \
@@ -138,26 +147,41 @@ do
 done
 
 # enable outbound TCP traffic to remote TCP servers
-echo "# enable outbound TCP traffic to remote TCP servers"
 for SVR_PORT in $REMOTE_TCP_SVRS
 do
-	$IPT -A INPUT -i $WAN_NIC -p tcp \
-				-s $ANY_ADDR --sport $SVR_PORT \
-				-d $HOST_ADDR --dport $UNPRIV_PORTS \
+	# $IPT -A INPUT -i $WAN_NIC -p tcp \
+	# 			-s $ANY_ADDR --sport $SVR_PORT \
+	# 			-d $HOST_ADDR --dport $UNPRIV_PORTS \
+	# 			-m state --state ESTABLISHED -j TCP_CLNT
+	# $IPT -A OUTPUT -o $WAN_NIC -p tcp \
+	# 			-d $ANY_ADDR --dport $SVR_PORT \
+	# 			-s $HOST_ADDR --sport $UNPRIV_PORTS \
+	# 			-m state --state NEW,ESTABLISHED -j TCP_CLNT
+	$IPT -A FORWARD -p tcp \
+				-i $WAN_NIC -s $ANY_ADDR --sport $SVR_PORT \
+				-o $LAN_NIC -d $SUBNET_ADDR --dport $UNPRIV_PORTS \
 				-m state --state ESTABLISHED -j TCP_CLNT
-	$IPT -A OUTPUT -o $WAN_NIC -p tcp \
-				-d $ANY_ADDR --dport $SVR_PORT \
-				-s $HOST_ADDR --sport $UNPRIV_PORTS \
-				-m state --state NEW,ESTABLISHED -j TCP_CLNT
-	$IPT -A FORWARD -i $WAN_NIC -o $LAN_NIC -p tcp \
-				-s $ANY_ADDR --sport $SVR_PORT \
-				-d $SUBNET_ADDR --dport $UNPRIV_PORTS \
-				-m state --state ESTABLISHED -j TCP_CLNT
-	$IPT -A FORWARD -i $LAN_NIC -o $WAN_NIC -p tcp \
-				-d $ANY_ADDR --dport $SVR_PORT \
-				-s $SUBNET_ADDR --sport $UNPRIV_PORTS \
+	$IPT -A FORWARD -p tcp \
+				-i $LAN_NIC -s $SUBNET_ADDR --sport $UNPRIV_PORTS \
+				-o $WAN_NIC -d $ANY_ADDR --dport $SVR_PORT \
 				-m state --state NEW,ESTABLISHED -j TCP_CLNT
 done
+
+# enable outbound UDP traffic to remote UDP servers
+for SVR_PORT in $REMOTE_UDP_SVRS
+do
+	$IPT -A FORWARD -p udp \
+				-i $WAN_NIC -s $ANY_ADDR --sport $SVR_PORT \
+				-o $LAN_NIC -d $SUBNET_ADDR --dport $UNPRIV_PORTS \
+				-j UDP_CLNT
+	$IPT -A FORWARD -p udp \
+				-i $LAN_NIC -s $SUBNET_ADDR --sport $UNPRIV_PORTS \
+				-o $WAN_NIC -d $ANY_ADDR --dport $SVR_PORT \
+				-j UDP_CLNT
+done
+
+# questions:
+# drop all packets destined for the firewall from the outside??? including established TCP connections? how about UDP packets?
 
 # ## SSH
 # $IPT -A OUTPUT -p tcp --dport 22 -j ssh
