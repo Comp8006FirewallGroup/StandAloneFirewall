1a28ad695c7113a672eb567f200ef4747d01e3bb
Eric Tsang <surplus.et@gmail.com>
2016-01-30 17:12:56 -0800

    firewall configures what dns servers the computer will use as well as restrict access to DNS servers..
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- firewall.sh
+++ firewall.sh
@@ -1,32 +1,56 @@
-#!/bin/sh
+# !/bin/sh
+echo "# !/bin/sh"
 
-#general
+### configuration ###
+echo "### configuration ###"
+
+# general
+echo "# general"
 IPT="iptables"
-ENABLE_DHCP="1"
+ALLOWED_ICMP_SERVICES="???"
 
 PS4='$LINENO '
 
-#IPS
-INTERNET="wlp2s0"
-IPADDR="10.64.205.150"
+# network interfaces
+echo "# network interfaces"
+WAN_NIC="wlan0"
+LAN_NIC="eth0"
+
+# addresses
+echo "# addresses"
+HOST_ADDR="192.168.1.74"
+SUBNET_ADDR="10.210.0.0/24"
+DHCP_SERVERS="192.168.1.254"
+NAME_SERVERS="192.168.1.254 8.8.8.8"
+
+# ports
+echo "# ports"
+UNPRIV_PORTS="1024:65535"
+PRIV_PORTS="0:1023"
+
+LOCAL_TCP_SVRS="22 80"
+REMOTE_TCP_SVRS="22 80 443"
+
+LOCAL_UDP_SERVERS="22 80"
+REMOTE_UDP_SERVERS="22 80 443"
+
+### implementation - do not touch! ###
+echo "### implementation - do not touch! ###"
+
+# addresses
+echo "# addresses"
 LOOPBACK="127.0.0.1"
-NAMESERVER="0.0.0.0/0"
+ANY_ADDR="0.0.0.0/0"
+BROADCAST_SRC_ADDR="0.0.0.0"
+BROADCAST_DEST_ADDR="255.255.255.255"
 
-#chains
-USER_CHAINS="ssh www"
-
-#ports
-UNPRIVPORTS="1024:65535"
-SSH_PORTS="22"
-DHCP_PORT="67:68"
-DNS_PORT="53"
-
-
+# reset firewall
+echo "# reset firewall"
 $IPT -F
 $IPT -X
-$IPT --policy INPUT ACCEPT
-$IPT --policy OUTPUT ACCEPT
-$IPT --policy FORWARD ACCEPT
+$IPT -P INPUT ACCEPT
+$IPT -P OUTPUT ACCEPT
+$IPT -P FORWARD ACCEPT
 
 if [ "$1" = "stop" ]
 then
@@ -34,86 +58,146 @@ then
 	exit 0
 fi
 
+# set default chain policies
+echo "# set default chain policies"
+$IPT -P INPUT DROP
+$IPT -P OUTPUT DROP
+$IPT -P FORWARD DROP
 
-$IPT --policy INPUT DROP
-$IPT --policy OUTPUT DROP
-$IPT --policy FORWARD DROP
+# configure DNS servers
+echo "nameserver $NAME_SERVERS" > /etc/resolv.conf
 
-#Create user tables
-for UC in $USER_CHAINS
+# create user chains
+echo "# create user chains"
+USER_CHAINS="DHCP DNS ICMP TCP_SVR TCP_CLNT"
+for CHAIN in $USER_CHAINS
 do
-	$IPT -N $UC
+	$IPT -N $CHAIN
+	$IPT -A $CHAIN -j ACCEPT
 done
 
-if [ $ENABLE_DHCP = "1" ]
-then
-	$IPT -A OUTPUT -o $INTERNET -p udp \
-				--dport 67:68 \
-				--sport $UNPRIVPORTS \
-				-j ACCEPT
-	$IPT -A INPUT -i $INTERNET -p udp \
-				--sport 67:68 \
-				--dport $UNPRIVPORTS \
-				-j ACCEPT
-fi
-
-
-#catch all set nameservers
-for NS in $NAMESERVER
+# enable DHCP traffic to DHCP servers
+echo "# enable DHCP traffic to DHCP servers"
+for SVR in $DHCP_SERVERS
 do
-$IPT -A OUTPUT -o $INTERNET -p udp \
-			--dport $DNS_PORT \
-			--sport $UNPRIVPORTS \
-			-j ACCEPT
-
-$IPT -A INPUT -i $INTERNET -p udp \
-			--sport $DNS_PORT \
-			--dport $UNPRIVPORTS \
-			-j ACCEPT
+	$IPT -A OUTPUT -o $WAN_NIC -p udp \
+				-s $BROADCAST_SRC_ADDR --sport 67:68 \
+				-d $BROADCAST_DEST_ADDR --dport 67:68 \
+				-j DHCP
+	$IPT -A INPUT -i $WAN_NIC -p udp \
+				-s $SVR --sport 67 \
+				-d $SUBNET_ADDR --dport 68 \
+				-j DHCP
+	$IPT -A OUTPUT -o $WAN_NIC -p udp \
+				-d $SVR --dport 67 \
+				-s $HOST_ADDR --sport 68 \
+				-j DHCP
+	$IPT -A INPUT -i $WAN_NIC -p udp \
+				-s $SVR --sport 67 \
+				-d $HOST_ADDR --dport 68 \
+				-j DHCP
 done
 
-## SSH
-$IPT -A OUTPUT -p tcp --dport 22 -j ssh
-$IPT -A INPUT  -p tcp --sport 22 -j ssh
+# enable DNS traffic to DNS servers
+echo "# enable DNS traffic to DNS servers"
+for SVR in $NAME_SERVERS
+do
+	$IPT -A OUTPUT -o $WAN_NIC -p udp \
+				-d $SVR --dport 53 \
+				-s $HOST_ADDR --sport $UNPRIV_PORTS \
+				-j DNS
+	$IPT -A INPUT -i $WAN_NIC -p udp \
+				-s $SVR --sport 53 \
+				-d $HOST_ADDR --dport $UNPRIV_PORTS \
+				-j DNS
+	$IPT -A FORWARD -o $WAN_NIC -p udp \
+				-d $SVR --dport 53 \
+				-s $SUBNET_ADDR --sport $UNPRIV_PORTS \
+				-j DNS
+	$IPT -A FORWARD -i $WAN_NIC -p udp \
+				-s $SVR --sport 53 \
+				-d $SUBNET_ADDR --dport $UNPRIV_PORTS \
+				-j DNS
+done
+
+# enable inbound TCP traffic to local TCP servers
+echo "# enable inbound TCP traffic to local TCP servers"
+for SVR_PORT in $LOCAL_TCP_SVRS
+do
+	$IPT -A INPUT -i $WAN_NIC -p tcp \
+				-d $HOST_ADDR --dport $SVR_PORT \
+				-s $ANY_ADDR --sport $UNPRIV_PORTS \
+				-m state --state NEW,ESTABLISHED -j TCP_SVR
+	$IPT -A OUTPUT -o $WAN_NIC -p tcp \
+				-s $HOST_ADDR --sport $SVR_PORT \
+				-d $ANY_ADDR --dport $UNPRIV_PORTS \
+				-m state --state ESTABLISHED -j TCP_SVR
+done
+
+# enable outbound TCP traffic to remote TCP servers
+echo "# enable outbound TCP traffic to remote TCP servers"
+for SVR_PORT in $REMOTE_TCP_SVRS
+do
+	$IPT -A INPUT -i $WAN_NIC -p tcp \
+				-s $ANY_ADDR --sport $SVR_PORT \
+				-d $HOST_ADDR --dport $UNPRIV_PORTS \
+				-m state --state ESTABLISHED -j TCP_CLNT
+	$IPT -A OUTPUT -o $WAN_NIC -p tcp \
+				-d $ANY_ADDR --dport $SVR_PORT \
+				-s $HOST_ADDR --sport $UNPRIV_PORTS \
+				-m state --state NEW,ESTABLISHED -j TCP_CLNT
+	$IPT -A FORWARD -i $WAN_NIC -o $LAN_NIC -p tcp \
+				-s $ANY_ADDR --sport $SVR_PORT \
+				-d $SUBNET_ADDR --dport $UNPRIV_PORTS \
+				-m state --state ESTABLISHED -j TCP_CLNT
+	$IPT -A FORWARD -i $LAN_NIC -o $WAN_NIC -p tcp \
+				-d $ANY_ADDR --dport $SVR_PORT \
+				-s $SUBNET_ADDR --sport $UNPRIV_PORTS \
+				-m state --state NEW,ESTABLISHED -j TCP_CLNT
+done
+
+# ## SSH
+# $IPT -A OUTPUT -p tcp --dport 22 -j ssh
+# $IPT -A INPUT  -p tcp --sport 22 -j ssh
 
 
-$IPT -A ssh -o $INTERNET -p tcp \
-			--sport $UNPRIVPORTS \
-			-d $IPADDR --dport 22 -j ACCEPT
+# $IPT -A ssh -o $WAN_NIC -p tcp \
+# 			--sport $UNPRIV_PORTS \
+# 			-d $HOST_ADDR --dport 22 -j ACCEPT
 
-$IPT -A ssh -i $INTERNET -p tcp \
-		 -s $IPADDR --sport 22 \
-		 --dport $UNPRIVPORTS -j ACCEPT
+# $IPT -A ssh -i $WAN_NIC -p tcp \
+# 		 -s $HOST_ADDR --sport 22 \
+# 		 --dport $UNPRIV_PORTS -j ACCEPT
 
-## WWW
-$IPT -A OUTPUT -m tcp -p tcp --sport 80 -j www
-$IPT -A INPUT  -m tcp -p tcp --sport 80 -j www
-$IPT -A OUTPUT -m tcp -p tcp --dport 443 -j www
-$IPT -A INPUT  -m tcp -p tcp --sport 443 -j www
+# ## WWW
+# $IPT -A OUTPUT -m tcp -p tcp --sport 80 -j www
+# $IPT -A INPUT  -m tcp -p tcp --sport 80 -j www
+# $IPT -A OUTPUT -m tcp -p tcp --dport 443 -j www
+# $IPT -A INPUT  -m tcp -p tcp --sport 443 -j www
 
-$IPT -A www -o $INTERNET -m tcp -p tcp \
-			-s $IPADDR --sport $UNPRIVPORTS \
-			--dport 80 -j ACCEPT
+# $IPT -A www -o $WAN_NIC -m tcp -p tcp \
+# 			-s $HOST_ADDR --sport $UNPRIV_PORTS \
+# 			--dport 80 -j ACCEPT
 
-$IPT -A www -i $INTERNET -m tcp -p tcp \
-			--sport 80 \
-			-d $IPADDR --dport $UNPRIVPORTS -j ACCEPT
+# $IPT -A www -i $WAN_NIC -m tcp -p tcp \
+# 			--sport 80 \
+# 			-d $HOST_ADDR --dport $UNPRIV_PORTS -j ACCEPT
 
-$IPT -A www -i 	$INTERNET -m tcp -p tcp \
-			--sport $UNPRIVPORTS \
-			-d $IPADDR --dport 80 -j DROP
+# $IPT -A www -i 	$WAN_NIC -m tcp -p tcp \
+# 			--sport $UNPRIV_PORTS \
+# 			-d $HOST_ADDR --dport 80 -j DROP
 
-$IPT -A www -o $INTERNET -m tcp -p tcp \
-			-s $IPADDR --sport $UNPRIVPORTS \
-			--dport 443 -j ACCEPT
+# $IPT -A www -o $WAN_NIC -m tcp -p tcp \
+# 			-s $HOST_ADDR --sport $UNPRIV_PORTS \
+# 			--dport 443 -j ACCEPT
 
-$IPT -A www -i $INTERNET -m tcp -p tcp \
-			--sport 443 \
-			-d $IPADDR --dport $UNPRIVPORTS -j ACCEPT
+# $IPT -A www -i $WAN_NIC -m tcp -p tcp \
+# 			--sport 443 \
+# 			-d $HOST_ADDR --dport $UNPRIV_PORTS -j ACCEPT
 
-$IPT -A www -i 	$INTERNET -m tcp -p tcp \
-			--sport $UNPRIVPORTS \
-			-d $IPADDR --dport 443 -j DROP
+# $IPT -A www -i 	$WAN_NIC -m tcp -p tcp \
+# 			--sport $UNPRIV_PORTS \
+# 			-d $HOST_ADDR --dport 443 -j DROP
 
 
-$IPT -A www
+# $IPT -A www
